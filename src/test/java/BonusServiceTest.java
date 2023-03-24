e53eda85df30e4004a0e655aeb45e78addf9416f
Willian-Reinaldo <willian.reinaldo.m@gmail.com>
2023-03-24 07:41:50 -0300

    add tests TDD

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,25 @@
+import br.com.alura.tdd.modelo.Funcionario;
+import br.com.alura.tdd.service.BonusService;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BonusServiceTest {
+
+    @Test
+    void BonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto(){
+        BonusService service = new BonusService();
+       BigDecimal bonus =  service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal(25000)));
+        assertEquals(new BigDecimal("0.00"), bonus);
+    }
+
+    @Test
+    void BonusDeveriaSerDezPorCentoParaSalarioDeExatamente10000(){
+        BonusService service = new BonusService();
+        BigDecimal bonus =  service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal(10000)));
+        assertEquals(new BigDecimal("1000.00"), bonus);
+    }
+}
